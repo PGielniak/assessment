6aefd48c83a62a57a9b94620e0bafd4d6e38a4e1
Patryk Gielniak <patryk@DESKTOP-L0OGG0A.localdomain>
2025-08-19 14:46:30 +0200

    remove unnecessary variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,3 @@
-variable "client_id" {}
-variable "client_secret" {}
-variable "tenant_id" {}
-variable "subscription_id" {}
-
 
 variable "environment" {
   description = "Environment name (dev, staging, prod)"
